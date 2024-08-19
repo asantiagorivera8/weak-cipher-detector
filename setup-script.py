ed8f0ae47121c15ece90f21a4c163b8f11d7bca4
asantiagorivera8 <161373289+asantiagorivera8@users.noreply.github.com>
2024-08-19 09:23:48 -0400

    Update setup-script.py

--- setup-script.py
+++ setup-script.py
@@ -13,11 +13,11 @@ setup(
         ],
     },
     author="Angel D. Santiago Rivera",
-    author_email="your.email@example.com",  # Replace with your actual email
+    author_email="adsr.20@gmail.com",  # Replace with your actual email
     description="A tool to detect weak ciphers in SSL/TLS certificates",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/weak-cipher-detector",  # Replace with your actual GitHub repository URL
+    url="https://github.com/asantiagorivera8/weak-cipher-detector",  # Replace with your actual GitHub repository URL
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
